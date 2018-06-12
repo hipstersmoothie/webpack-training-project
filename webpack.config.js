5c0bc16c4fc9d22bcff420a7630d5f04bfbc259b
Andrew Lisowski <andrew_lisowski@intuit.com>
2018-06-12 15:41:12 -0700

    uglify and splits vendors. bundle now < 160kb

--- webpack.config.js
+++ webpack.config.js
@@ -12,58 +12,119 @@
  */
 
 const path = require('path');
+
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
+
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  mode: 'development',
+  mode: isProduction ? 'development' : 'production',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'public', 'build'),
     filename: 'bundle.js',
-    publicPath: '/build/',
+    publicPath: '/build/'
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          parse: {
+            // We want uglify-js to parse ecma 8 code. However, we don't want it
+            // to apply any minfication steps that turns valid ecma 5 code
+            // into invalid ecma 5 code. This is why the 'compress' and 'output'
+            // sections only apply transformations that are ecma 5 safe
+            // https://github.com/facebook/create-react-app/pull/4234
+            ecma: 8
+          },
+          compress: {
+            ecma: 5,
+            warnings: false,
+            // Disabled because of an issue with Uglify breaking seemingly valid code:
+            // https://github.com/facebook/create-react-app/issues/2376
+            // Pending further investigation:
+            // https://github.com/mishoo/UglifyJS2/issues/2011
+            comparisons: false
+          },
+          minify: {
+            toplevel: true
+          },
+          mangle: {
+            safari10: true
+          },
+          output: {
+            ecma: 5,
+            comments: false,
+            // Turned on because emoji and regex is not minified properly using default
+            // https://github.com/facebook/create-react-app/issues/2488
+            // eslint-disable-next-line camelcase
+            ascii_only: true
+          }
+        },
+        // Use multi-process parallel running to improve the build speed
+        // Default number of concurrent runs: os.cpus().length - 1
+        parallel: true,
+        // Enable file caching
+        cache: true
+      })
+    ],
+    splitChunks: {
+      chunks: 'all',
+      name: 'vendors'
+    }
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        loader: 'babel-loader'
         // Babel options are loaded from .babelrc
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
       {
         test: /\.svg$/,
-        loader: 'file-loader',
-      },
-    ],
+        loader: 'file-loader'
+      }
+    ]
   },
   plugins: [
     // Emit HTML files that serve the app
     new HtmlWebpackPlugin({
       template: 'src/templates/landing.html',
       filename: path.resolve(__dirname, 'public/index.html'),
-      alwaysWriteToDisk: true,
+      alwaysWriteToDisk: true
     }),
     new HtmlWebpackPlugin({
       template: 'src/templates/app.html',
       filename: path.resolve(__dirname, 'public/users/index.html'),
-      alwaysWriteToDisk: true,
+      alwaysWriteToDisk: true
     }),
+    new BundleAnalyzerPlugin(),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
   ].concat(
     isProduction
       ? []
       : [
           // Force writing the HTML files to disk when running in the development mode
           // (otherwise, webpack-dev-server won’t serve the app)
-          new HtmlWebpackHarddiskPlugin(),
-        ],
+          new HtmlWebpackHarddiskPlugin()
+        ]
   ),
   devServer: {
-    contentBase: path.join(__dirname, 'public'),
-  },
+    contentBase: path.join(__dirname, 'public')
+  }
 };
